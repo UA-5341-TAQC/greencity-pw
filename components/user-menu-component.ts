1bfe30db055795624e4d90bddff1c4f8e0e1fc8a
Svitlana-Kovalova <svitlanakovalova1008@gmail.com>
2026-09-21 17:01:46 +0300

    Update user menu locators

--- components/user-menu-component.ts
+++ components/user-menu-component.ts
@@ -18,17 +18,17 @@ export class UserMenuComponent extends BaseComponent {
 
     this.dropdownMenu = this.root.locator('.dropdown-list');
 
-    this.notificationsItem = this.root.getByRole('listitem', {
-      name: 'notifications',
-    });
+    this.notificationsItem = this.root.locator(
+      'li[aria-label="notifications"]'
+    );
 
-    this.personalAccountLink = this.root.getByRole('link', {
-      name: 'Personal account',
-    });
+    this.personalAccountLink = this.root.locator(
+      'a[href="#/ubs/user/orders"]'
+    );
 
-    this.signOutButton = this.root.getByRole('button', {
-      name: 'sign-out',
-    });
+    this.signOutButton = this.root.locator(
+      'li[aria-label="sign-out"]'
+    );
   }
 
   /** Opens the user menu. */
